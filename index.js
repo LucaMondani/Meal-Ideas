62f5b40f423d2a5f94136f1947136a3d78802c0c
Luca Mondani <luca.mondani@magenta.de>
2024-08-11 08:52:03 +0200

    Add /dessert route.

--- index.js
+++ index.js
@@ -61,8 +61,33 @@ app.get("/vegetarian", async (req, res) => {
     
 });
 
-app.get("/dessert", (req, res) => {
-    res.render("index.ejs", {content: "Ne"});
+app.get("/dessert", async (req, res) => {
+    try {
+        const response = await axios.get("https://www.themealdb.com/api/json/v1/1/filter.php?c=Dessert", config);
+        const result = response.data;
+        let meals = result["meals"];
+        let randomMeal = meals[Math.floor(Math.random() * meals.length)];
+        let randomMealName = randomMeal["strMeal"];
+        try {
+            const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/search.php?s=${randomMealName}`, config);
+            const result = response.data;
+            const path = result["meals"][0];
+            let name = path["strMeal"];
+            let origin = path["strArea"];
+            let category = path["strCategory"];
+            let instructions = path["strInstructions"];
+            let image = path["strMealThumb"];
+            let tutorial = path["strYoutube"];
+            let recipe = path["strSource"];
+            res.render("index.ejs", {name: name, origin: origin, category: category, instructions: instructions, image: image, tutorial:tutorial, recipe:recipe});
+        } catch (error) {
+            console.log(error)
+            res.render("index.ejs", {content: error});
+        }
+    } catch (error) {
+        console.log(error)
+        res.render("index.ejs", {content: error});
+    }
 }); 
 
 app.listen(port, () => {
